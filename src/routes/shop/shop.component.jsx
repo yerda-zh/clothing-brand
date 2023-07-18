a47c1ff305d75176d003bcae7583bedc633a8731
yerda-zh <yerdaulet.04@gmail.com>
2023-07-18 14:14:42 +0500

    changed redux thunk into redux saga

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,13 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 import { useDispatch } from 'react-redux';
-import { fetchCategoriesAsync } from '../../store/categories/category.action';
+import { fetchCategoriesStart } from '../../store/categories/category.action';
 
 const Shop =() => {
     const dispatch = useDispatch();
 
     useEffect(()=>{
-        dispatch(fetchCategoriesAsync());
+        dispatch(fetchCategoriesStart());
       }, []);
     
     return(
